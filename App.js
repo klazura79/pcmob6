894f2e151bae31e7346918dad0effd3a035852a8
Affan Noor <104568978+klazura79@users.noreply.github.com>
2022-09-19 22:16:49 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, StatusBar, StyleSheet, View } from "react-native";
+
+import HomeStack from "./components/HomeStack";
+import { AUTH_SCREEN, HOME_STACK } from "./constants";
+
+import AuthScreen from "./screens/AuthScreen";
+import ContactScreen from "./screens/ContactScreen";
+const Stack = createStackNavigator();
+import { Provider } from "react-redux";
+import store from "./store";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AppSource />
+    </Provider>
+  );
+  
+}
+
+function AppSource() {
+  const [loading, setLoading] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  async function setToken() {
+    const token = await AsyncStorage.getItem("token");
+    // if there is not token, it will be falsey value
+    setLoggedIn(token);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setToken();
+  }, []);
+
+  const LoadingScreen = () => (
+    <View style={styles.container}>
+      <ActivityIndicator />
+    </View>
+  );
+  const AppScreen = () => (
+    <NavigationContainer>
+      <StatusBar />
+      <Stack.Navigator
+        initialRouteName={loggedIn ? HOME_STACK : AUTH_SCREEN}
+        screenOptions={{
+          animationEnabled: false,
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen component={AuthScreen} name={AUTH_SCREEN} />
+        <Stack.Screen component={HomeStack} name={HOME_STACK} />
+        
+        
+       
+      </Stack.Navigator>
+     
+    </NavigationContainer>
+  );
+  return loading ? <LoadingScreen /> : <AppScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffde59",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  
+});
